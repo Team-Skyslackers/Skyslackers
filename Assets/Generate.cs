57c385a4cd4f22ddab99acdce41fbb5888c9cd2e
Zhang Haoyu <haoyusimon927@gmail.com>
2021-06-18 22:17:58 +0800

    new material & improved light saber experience

--- Assets/Generate.cs
+++ Assets/Generate.cs
@@ -6,6 +6,7 @@ public class Generate : MonoBehaviour
 {
     private float nextActionTime = 0.0f;
     public float period = 2.0f;
+    public GameObject beam;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,19 +19,20 @@ public class Generate : MonoBehaviour
         if (Time.time > nextActionTime ) {
             nextActionTime += period;
 
-            GameObject piece;
-            piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            // GameObject piece;
+            // piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
             
-            //set piece position and scale
-            piece.transform.position = new Vector3(-4.8f, 4.56f, 40.0f);
-            piece.transform.localScale = new Vector3(1, 1, 5);
+            // //set piece position and scale
+            // piece.transform.position = new Vector3(-4.8f, 4.56f, 40.0f);
+            // piece.transform.localScale = new Vector3(1, 1, 5);
 
             //add rigidbody and set mass
-            piece.AddComponent<Rigidbody>();
-            piece.GetComponent<Rigidbody>().mass = 100000000;
-            piece.AddComponent<Explosion>();
-            piece.GetComponent<Rigidbody>().useGravity = false;
-            
+            // piece.AddComponent<Rigidbody>();
+            // piece.GetComponent<Rigidbody>().mass = 100000000;
+            // piece.AddComponent<Explosion>();
+            // piece.GetComponent<Rigidbody>().useGravity = false;
+            Instantiate(beam, new Vector3(-4.8f, 4.56f, 40.0f), Quaternion.identity);
+            //beam.AddComponent<Explosion>();
         }
     }
 }
