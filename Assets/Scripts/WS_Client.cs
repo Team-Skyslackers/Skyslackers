49b5b6bd131209c6589573ddbbf635c86d5be66e
Yue Junfeng <yuejunfeng0909@gmail.com>
2021-06-20 18:13:23 +0800

    adjust position of lightsaber based on gyro info

--- Assets/Scripts/WS_Client.cs
+++ Assets/Scripts/WS_Client.cs
@@ -7,6 +7,8 @@ public class WS_Client : MonoBehaviour
 {
     public GameObject lightsaber;
     private float x = 0, y = 0, z = 0;
+    private float raw_x = 0, raw_y = 0, raw_z = 0;
+    private float pos_x = 0, pos_y = 0;
     WebSocket ws;
     private void Start()
     {
@@ -15,15 +17,21 @@ public class WS_Client : MonoBehaviour
         ws.OnMessage += (sender, e) =>
         {
             Debug.Log(e.Data);
-            y = -float.Parse(e.Data.Split(' ')[0]);
-            x = 90-float.Parse(e.Data.Split(' ')[1]);
+            raw_x = float.Parse(e.Data.Split(' ')[1]);
+            raw_y = float.Parse(e.Data.Split(' ')[0]);
+            y = -raw_y;
+            x = 90-raw_x;
             //z = -float.Parse(e.Data.Split(' ')[2]);
         };
     }
     void Update()
     {
         lightsaber.transform.rotation = Quaternion.Euler(x, y, z);
-        //lightsaber.transform.Translate(-0.01f,0f,0f);
+        pos_x = (raw_y < 180)? ((raw_y > 90)? raw_y - 180.0f:-raw_y): ((raw_y < 270)? raw_y - 180.0f :360.0f - raw_y);
+        pos_y = (raw_x > 90)? 180.0f-raw_x:((raw_x<-90)? -180.0f - raw_x:raw_x);
+        pos_x = (raw_x > 90 || raw_x < -90)? -pos_x:pos_x;
+        lightsaber.transform.position = new Vector3(pos_x/9.0f, pos_y/9.0f,0);
+        // lightsaber.transform.Translate(-0.01f,0f,0f);
         // float posx = 0, posy = 0;
         // if(x <= 90){
         //     posx = x * 20.0 / 90.0;
