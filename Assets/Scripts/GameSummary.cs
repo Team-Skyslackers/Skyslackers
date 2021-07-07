65016d4a9f3f6564f9de343bb6934f7f1c9a18da
Yue Junfeng <yuejunfeng0909@gmail.com>
2021-07-08 00:06:39 +0800

    Some basic communication
    can process UID info send from phone
    can package and send game summary

--- Assets/Scripts/GameSummary.cs
+++ Assets/Scripts/GameSummary.cs
@@ -1,13 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Firebase.Auth;
+using Firebase.Storage;
+using Firebase.Database;
+
+
 
 public class GameSummary : MonoBehaviour
 {
     static public int perfect, good, missed;
     public Text totalScore, perfectText, goodText, missedText;
     public GameObject SummaryCanvas;
+    public GameObject WS_manager;
+    public struct GameResults
+    {
+        public string uid;
+        public int perfect;
+        public int good;
+        public int missed;
+        public int total;
+        public string dateAndTimeUTC;
+        public GameResults(int _total, int _perfect, int _good, int _missed)
+        {
+            uid = "placeholder";
+            total = _total;
+            perfect = _perfect;
+            good = _good;
+            missed = _missed;
+            dateAndTimeUTC = DateTime.UtcNow.ToString("s");
+        }
+    }
 
     static public bool showingSummary = false;
 
@@ -19,8 +44,9 @@ public class GameSummary : MonoBehaviour
     void Update()
     {
         //Debug.Log(Generate.music_current_time.ToString("N") + " / " + Generate.totalMusicLength.ToString("N"));
-        if (Generate.music_current_time >= Generate.totalMusicLength - 1)
+        if (Generate.music_current_time >= Generate.totalMusicLength - 1 && showingSummary == false)
         {
+            // only run once when the game ends
             Cursor.visible = true;
             showingSummary = true;
             totalScore.text = Generate.myScore.ToString();
@@ -28,6 +54,9 @@ public class GameSummary : MonoBehaviour
             goodText.text = good.ToString();
             missedText.text = missed.ToString();
             SummaryCanvas.SetActive(true);
+
+            GameResults gameResults = new GameResults(Generate.myScore, perfect, good, missed);
+            WS_manager.GetComponent<WS_Client>().ws.Send("Summary " + JsonUtility.ToJson(gameResults).ToString()) ;
         }
     }
 }
---
+++ Assets/Scripts/displayUID.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayUID : MonoBehaviour
+{
+    public Text UIDText;
+    public GameObject WS_manager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIDText.text = WS_manager.GetComponent<WS_Client>().UID;
+    }
+}
